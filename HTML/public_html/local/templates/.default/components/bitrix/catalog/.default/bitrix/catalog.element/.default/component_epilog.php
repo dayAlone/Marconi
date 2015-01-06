e4dd87aab6df6e40d3d83a3f296de5457c71b463
dayAlone <andrey.slider@gmail.com>
2015-01-06 17:58:26 +0500

    Changes

--- HTML/public_html/local/templates/.default/components/bitrix/catalog/.default/bitrix/catalog.element/.default/component_epilog.php
+++ HTML/public_html/local/templates/.default/components/bitrix/catalog/.default/bitrix/catalog.element/.default/component_epilog.php
@@ -1,3 +1,20 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 $APPLICATION->SetPageProperty('body_class', "product");
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $templateData */
+/** @var @global CMain $APPLICATION */
+use Bitrix\Main\Loader;
+global $APPLICATION;
+if (isset($templateData['JS_OBJ']))
+{
+?><script type="text/javascript">
+BX.ready(BX.defer(function(){
+	if (!!window.<? echo $templateData['JS_OBJ']; ?>)
+	{
+		window.<? echo $templateData['JS_OBJ']; ?>.allowViewedCount(true);
+	}
+}));
+</script><?
+}
+?>
 ?>
--- HTML/public_html/local/templates/.default/components/bitrix/catalog/.default/bitrix/catalog.element/.default/result_modifier.php
+++ HTML/public_html/local/templates/.default/components/bitrix/catalog/.default/bitrix/catalog.element/.default/result_modifier.php
@@ -13,5 +13,5 @@
 
 	$small = CFile::ResizeImageGet(CFile::GetFileArray($arResult['PREVIEW_PICTURE']['ID']), Array("width" => 800, "height" => 800), BX_RESIZE_IMAGE_PROPORTIONAL, false, Array("name" => "sharpen", "precision" => 15), false, 75);
 	$arResult['PREVIEW_PICTURE']['SMALL'] = $small['src'];
-
+	
 ?>
