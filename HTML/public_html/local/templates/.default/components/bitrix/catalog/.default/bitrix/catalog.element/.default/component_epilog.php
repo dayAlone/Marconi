177dc5d752eb4e6a7e16be0a1b1730ab2e01fbe2
dayAlone <andrey.slider@gmail.com>
2015-01-06 18:38:10 +0500

    Changes

--- HTML/public_html/local/templates/.default/components/bitrix/catalog/.default/bitrix/catalog.element/.default/component_epilog.php
+++ HTML/public_html/local/templates/.default/components/bitrix/catalog/.default/bitrix/catalog.element/.default/component_epilog.php
@@ -4,6 +4,25 @@ $APPLICATION->SetPageProperty('body_class', "product");
 /** @var @global CMain $APPLICATION */
 use Bitrix\Main\Loader;
 global $APPLICATION;
+if (isset($templateData['TEMPLATE_THEME']))
+{
+	$APPLICATION->SetAdditionalCSS($templateData['TEMPLATE_THEME']);
+}
+if (isset($templateData['TEMPLATE_LIBRARY']) && !empty($templateData['TEMPLATE_LIBRARY']))
+{
+	$loadCurrency = false;
+	if (!empty($templateData['CURRENCIES']))
+		$loadCurrency = Loader::includeModule('currency');
+	CJSCore::Init($templateData['TEMPLATE_LIBRARY']);
+	if ($loadCurrency)
+	{
+	?>
+	<script type="text/javascript">
+		BX.Currency.setCurrencies(<? echo $templateData['CURRENCIES']; ?>);
+	</script>
+<?
+	}
+}
 if (isset($templateData['JS_OBJ']))
 {
 ?><script type="text/javascript">
