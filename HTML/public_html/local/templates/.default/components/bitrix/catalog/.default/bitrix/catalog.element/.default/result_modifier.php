7fc30717c9d24a8204eed2c8f8156edd55e67fde
dayAlone <andrey.slider@gmail.com>
2015-01-06 17:50:07 +0500

    Changes

--- HTML/public_html/local/templates/.default/components/bitrix/catalog/.default/bitrix/catalog.element/.default/result_modifier.php
+++ HTML/public_html/local/templates/.default/components/bitrix/catalog/.default/bitrix/catalog.element/.default/result_modifier.php
@@ -12,6 +12,6 @@
 	endwhile;
 
 	$small = CFile::ResizeImageGet(CFile::GetFileArray($item['PREVIEW_PICTURE']['ID']), Array("width" => 800, "height" => 800), BX_RESIZE_IMAGE_PROPORTIONAL, false, Array("name" => "sharpen", "precision" => 15), false, 75);
-	$item['PREVIEW_PICTURE']['SMALL'] = $small['src'];
+	$arResult['PREVIEW_PICTURE']['SMALL'] = $small['src'];
 
 ?>
