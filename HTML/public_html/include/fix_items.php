d84098005c147e47365bc0b178c95c21df48f7bc
dayAlone <andrey.slider@gmail.com>
2015-01-09 00:13:14 +0500

    Changes

--- HTML/public_html/include/fix_items.php
+++ HTML/public_html/include/fix_items.php
@@ -41,53 +41,85 @@ foreach($arResult["ITEMS"] as $PID => $arItem)
 	}
 }
 
-$index = array();
-foreach ($arResult["COMBO"] as $id => $combination)
-	foreach ($combination as $PID => $value)
-		$index[$PID][$value][] = &$arResult["COMBO"][$id];
-
-$this->facet = new \Bitrix\Iblock\PropertyIndex\Facet($arResult['SECTION']['IBLOCK_ID']);
 if(count($allCHECKED)>0):
+	function condition($_CHECK, $ar)
+	{
+		if (isset($_CHECK[$ar["CONTROL_NAME"]])
+					|| (
+						isset($_CHECK[$ar["CONTROL_NAME_ALT"]])
+						&& $_CHECK[$ar["CONTROL_NAME_ALT"]] == $ar["HTML_VALUE_ALT"]
+					))
+			return true;
+		else
+			return false;
+	}
+	function apply(&$arResult, &$facetIndex, $el)
+	{
+		$res = $el->facet->query($arResult["FACET_FILTER"]);
+		while ($row = $res->fetch()):
+			$facetId = $row["FACET_ID"];
+			if (\Bitrix\Iblock\PropertyIndex\Storage::isPropertyId($facetId)):
+				$pp = \Bitrix\Iblock\PropertyIndex\Storage::facetIdToPropertyId($facetId);
+				if ($arResult["ITEMS"][$pp]["PROPERTY_TYPE"] != "N"):
+					if (isset($facetIndex[$pp][$row["VALUE"]])):
+							unset($facetIndex[$pp][$row["VALUE"]]["DISABLED"]);
+						endif;
+				endif;
+			endif;
+		endwhile;
+	}
 
-	$this->facet->setPrices($arResult["PRICES"]);
-	$this->facet->setSectionId($arResult['SECTION']['ID']);
+	$index = array();
+	foreach ($arResult["COMBO"] as $id => $combination)
+		foreach ($combination as $PID => $value)
+			$index[$PID][$value][] = &$arResult["COMBO"][$id];
 
 	$facetIndex = array();
 	$except     = getFilterProperties();
 
+	$directions = array();
 	foreach($arResult["ITEMS"] as $PID => $arItem):
 		foreach($arItem["VALUES"] as $key => $ar):
 			if ($arResult["FACET_FILTER"] && isset($ar["FACET_VALUE"]))
 				$facetIndex[$PID][$ar["FACET_VALUE"]] = &$arResult["ITEMS"][$PID]["VALUES"][$key];
-			if(
-				isset($_CHECK[$ar["CONTROL_NAME"]])
-				|| (
-					isset($_CHECK[$ar["CONTROL_NAME_ALT"]])
-					&& $_CHECK[$ar["CONTROL_NAME_ALT"]] == $ar["HTML_VALUE_ALT"]
-				)
-			):
-				if($_CHECK[$ar["CONTROL_NAME"]] == $ar["HTML_VALUE"] && !isset($arItem["PRICE"])):
-					if ($arResult["FACET_FILTER"] && !in_array($PID, $except)):
-						$this->facet->addDictionaryPropertyFilter($PID, "=", $ar["FACET_VALUE"]);
-					endif;
-				endif;
-			endif;
-
+			if(condition($_CHECK, $ar))
+				if(in_array($PID, $except))
+					$directions[] = $PID;
 		endforeach;
 	endforeach;
 
-	$res = $this->facet->query($arResult["FACET_FILTER"]);
-	while ($row = $res->fetch()):
-		$facetId = $row["FACET_ID"];
+	if(count($directions) > 0):
+		foreach ($directions as $direction):
+				$this->facet = new \Bitrix\Iblock\PropertyIndex\Facet($arResult['SECTION']['IBLOCK_ID']);
+				$this->facet->setPrices($arResult["PRICES"]);
+				$this->facet->setSectionId($arResult['SECTION']['ID']);
+				$value = false;
+				foreach($arResult["ITEMS"] as $PID => $arItem)
+					foreach($arItem["VALUES"] as $key => $ar)
+						if(condition($_CHECK, $ar))
+							if($_CHECK[$ar["CONTROL_NAME"]] == $ar["HTML_VALUE"] && !isset($arItem["PRICE"]))
+								if ($arResult["FACET_FILTER"] && !in_array($PID, $except))
+									$this->facet->addDictionaryPropertyFilter($PID, "=", $ar["FACET_VALUE"]);
+								else if($PID == $direction)
+									$value = $ar["FACET_VALUE"];
+				
+				$this->facet->addDictionaryPropertyFilter($direction, "=", $value);
+				
+				apply($arResult, $facetIndex, $this);
+			endforeach;
+	else:
+		$this->facet = new \Bitrix\Iblock\PropertyIndex\Facet($arResult['SECTION']['IBLOCK_ID']);
+		$this->facet->setPrices($arResult["PRICES"]);
+		$this->facet->setSectionId($arResult['SECTION']['ID']);
 		
-		if (\Bitrix\Iblock\PropertyIndex\Storage::isPropertyId($facetId)):
-			$pp = \Bitrix\Iblock\PropertyIndex\Storage::facetIdToPropertyId($facetId);
-			if ($arResult["ITEMS"][$pp]["PROPERTY_TYPE"] != "N"):
-				if (isset($facetIndex[$pp][$row["VALUE"]])):
-						unset($facetIndex[$pp][$row["VALUE"]]["DISABLED"]);
-					endif;
-			endif;
-		endif;
-	endwhile;
+		foreach($arResult["ITEMS"] as $PID => $arItem)
+			foreach($arItem["VALUES"] as $key => $ar)
+				if(condition($_CHECK, $ar))
+					if($_CHECK[$ar["CONTROL_NAME"]] == $ar["HTML_VALUE"] && !isset($arItem["PRICE"]))
+						if ($arResult["FACET_FILTER"])
+							$this->facet->addDictionaryPropertyFilter($PID, "=", $ar["FACET_VALUE"]);
+					
+		apply($arResult, $facetIndex, $this);
+	endif;
 endif;
 ?>
