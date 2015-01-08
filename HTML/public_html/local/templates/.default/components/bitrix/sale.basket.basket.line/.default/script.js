c5461e0aaec494f4ce07be690a492bacbeacbdf5
dayAlone <andrey.slider@gmail.com>
2015-01-08 19:47:09 +0500

    Changes

--- HTML/public_html/local/templates/.default/components/bitrix/sale.basket.basket.line/.default/script.js
+++ HTML/public_html/local/templates/.default/components/bitrix/sale.basket.basket.line/.default/script.js
@@ -167,6 +167,7 @@ BitrixSmallCart.prototype = {
 
 	setCartBody: function (result)
 	{
+		console.log(result)
 		if (BX(this.cartId))
 			BX(this.cartId).innerHTML = result;
 		if (this.fixedPosition)
