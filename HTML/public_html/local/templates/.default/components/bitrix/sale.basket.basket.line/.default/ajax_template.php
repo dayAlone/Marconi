6f30739da7b7e6c77a48f1ea7bae1c8048fa5984
dayAlone <andrey.slider@gmail.com>
2015-01-08 23:06:36 +0500

    Changes

--- HTML/public_html/local/templates/.default/components/bitrix/sale.basket.basket.line/.default/ajax_template.php
+++ HTML/public_html/local/templates/.default/components/bitrix/sale.basket.basket.line/.default/ajax_template.php
@@ -1,5 +1,13 @@
-<?if($arResult['NUM_PRODUCTS']>0):?>
+<?
+$frame = $this->createFrame()->begin();
+if($arResult['NUM_PRODUCTS']>0):?>
 	<a href="/basket/" class="cart"><?=svg('cart')?>В корзине: <span class="cart__num"><?=$arResult['NUM_PRODUCTS']?></span> <?=$arResult['PRODUCT(S)']?></a>
 <? else: ?>
 	<span class="cart"><?=svg('cart')?> Корзина пуста</span>
-<? endif;?>
+<? endif;
+$frame->beginStub();
+?>
+	<span class="cart"><?=svg('cart')?> Корзина пуста</span>
+<?
+$frame->end();
+?>
