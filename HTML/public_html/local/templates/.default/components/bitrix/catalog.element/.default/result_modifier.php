2090c0c2eec7a056befdcaaecfaf4c6f6908a354
dayAlone <andrey.slider@gmail.com>
2015-01-11 02:53:16 +0500

    Changes

--- HTML/public_html/local/templates/.default/components/bitrix/catalog.element/.default/result_modifier.php
+++ HTML/public_html/local/templates/.default/components/bitrix/catalog.element/.default/result_modifier.php
@@ -7,11 +7,9 @@
 	$arResult['IMAGES']    = array();
 	$raw = CFile::GetList(array(), array('@ID'=>implode($arResult['PROPERTIES']['PICTURES']['VALUE'],',')));
 	while($img = $raw->Fetch()):
-		if(file_exists($_SERVER['DOCUMENT_ROOT']."/upload/".$img['SUBDIR']."/".$img['FILE_NAME'])):
-			$small = CFile::ResizeImageGet(CFile::GetFileArray($img['ID']), Array("width" => 200, "height" => 200), BX_RESIZE_IMAGE_PROPORTIONAL, false, Array("name" => "sharpen", "precision" => 15), false, 75);
-			$middle = CFile::ResizeImageGet(CFile::GetFileArray($img['ID']), Array("width" => 800, "height" => 800), BX_RESIZE_IMAGE_PROPORTIONAL, false, Array("name" => "sharpen", "precision" => 15), false, 75);
-			$arResult['IMAGES'][] = array('small'=>$small['src'], 'middle'=>$middle['src'], 'src'=>"/upload/".$img['SUBDIR']."/".$img['FILE_NAME'], 'h'=>$img['HEIGHT'], 'w'=>$img['WIDTH']);
-		endif;
+		$small = CFile::ResizeImageGet(CFile::GetFileArray($img['ID']), Array("width" => 200, "height" => 200), BX_RESIZE_IMAGE_PROPORTIONAL, false, Array("name" => "sharpen", "precision" => 15), false, 75);
+		$middle = CFile::ResizeImageGet(CFile::GetFileArray($img['ID']), Array("width" => 800, "height" => 800), BX_RESIZE_IMAGE_PROPORTIONAL, false, Array("name" => "sharpen", "precision" => 15), false, 75);
+		$arResult['IMAGES'][] = array('small'=>$small['src'], 'middle'=>$middle['src'], 'src'=>"/upload/".$img['SUBDIR']."/".$img['FILE_NAME'], 'h'=>$img['HEIGHT'], 'w'=>$img['WIDTH']);
 	endwhile;
 	$small = CFile::ResizeImageGet(CFile::GetFileArray($arResult['PREVIEW_PICTURE']['ID']), Array("width" => 800, "height" => 800), BX_RESIZE_IMAGE_PROPORTIONAL, false, Array("name" => "sharpen", "precision" => 15), false, 75);
 	$arResult['PREVIEW_PICTURE']['SMALL'] = $small['src'];
