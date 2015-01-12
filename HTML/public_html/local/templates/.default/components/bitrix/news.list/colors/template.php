375d5a762c3ada64de1234525f36b0d9234eeb03
dayAlone <andrey.slider@gmail.com>
2015-01-13 01:25:22 +0500

    Changes

--- HTML/public_html/local/templates/.default/components/bitrix/news.list/colors/template.php
+++ HTML/public_html/local/templates/.default/components/bitrix/news.list/colors/template.php
@@ -2,7 +2,6 @@
 <? if(count($arResult['ITEMS'])>1): ?>
 <div class="colors">
 <?foreach ($arResult['ITEMS'] as $key=>$item):
-	var_dump($item['PROPERTIES']['PICTURES']['VALUE'][0]);
 	if(isset($item['PREVIEW_PICTURE']['SRC'])||$item['PROPERTIES']['PICTURES']['VALUE'][0]['small']):?>
 	<a style="background-image:url(<?=($item['PREVIEW_PICTURE']['SRC']?$item['PREVIEW_PICTURE']['SRC']:$item['PROPERTIES']['PICTURES']['VALUE'][0]['small'])?>)" href="<?=$item['DETAIL_PAGE_URL']?>" class="colors__item <?=($arParams['CACHE_NOTES']==$item['ID']?"colors__item--active":"")?>"></a>
 <?
