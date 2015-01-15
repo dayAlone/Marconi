383cacc99bb52260d5b4dced5f403b4c74c68f65
dayAlone <andrey.slider@gmail.com>
2015-01-15 23:03:54 +0500

    Changes

--- HTML/public_html/local/templates/.default/components/bitrix/system.pagenavigation/.default/template.php
+++ HTML/public_html/local/templates/.default/components/bitrix/system.pagenavigation/.default/template.php
@@ -11,14 +11,13 @@ $strNavQueryString = ($arResult["NavQueryString"] != "" ? str_replace('&amp;shor
 	<?
 	$i++;
 	endwhile;?>
-	<?/*
-	<?if ($arResult["bShowAll"]):?>
+	<?if ($arResult["bShowAll"] && $arResult["NavPageCount"]<10):?>
 		<?if ($arResult["NavShowAll"]):?>
 			<a href="<?=$arResult["sUrlPath"]?>?<?=$strNavQueryString?>SHOWALL_<?=$arResult["NavNum"]?>=0" rel="nofollow"><?=GetMessage("nav_paged")?></a>
 		<?else:?>
 			<a href="<?=$arResult["sUrlPath"]?>?<?=$strNavQueryString?>SHOWALL_<?=$arResult["NavNum"]?>=1" rel="nofollow" class="pages__item <?=($i==$arResult["NavPageNomer"]?'pages__item--active':"")?>">Показать все</a>
 		<?endif?>
 	<?endif?>
-	*/?>
+	
 </div>
 <?endif;?>
