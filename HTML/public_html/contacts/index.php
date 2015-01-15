a58b4f8c10999170525ac19d7736f6438bb44d72
dayAlone <andrey.slider@gmail.com>
2015-01-15 15:20:45 +0500

    Link

--- HTML/public_html/contacts/index.php
+++ HTML/public_html/contacts/index.php
@@ -2,16 +2,15 @@
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 if(!isset($_REQUEST['short'])):
 	$APPLICATION->SetPageProperty('body_class', "contacts");
-	$APPLICATION->SetTitle('Контакты');
-	?>
-	<div class="page">
+	$APPLICATION->SetTitle("Контакты");
+	?>	<div class="page">
 	    <div class="container">
 	    	<div class="row xl-margin-top">
 	    		<div class="col-md-5">
 	    			<div class="page__title">Контактная информация</div>
 	    			<p>
 	    				Телефон для справок и приема заказов: +7 926 468 86 86<br>
-						E-mail: info@fmarconi.ru <br>
+						E-mail: <a href="mailto:info@fmarconi.ru">info@fmarconi.ru</a> <br>
 						Адрес: 125212, г. Москва, ул. Адмирала Макарова, д. 8
 	    			</p>
 	    		</div>
@@ -25,7 +24,7 @@ if(!isset($_REQUEST['short'])):
 
 	    		</div>
 	    		<div class="col-md-2">
-	    			<a href="#feedback" data-toggle="modal" data-target="#feedback" class="product__big-button product__big-button--border full-width xxl-margin-bottom">Напишите нам</a>
+	    		<!--	<a href="#feedback" data-toggle="modal" data-target="#feedback" class="product__big-button product__big-button--border full-width xxl-margin-bottom">Напишите нам</a>-->
 	    			<p class="xxl-margin-top full-width">ООО «Мегатрон»</p>
 	    		</div>
 	    	</div>
