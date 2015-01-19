4f02d9ceb547f033b95afd15b9d5336db804dead
dayAlone <andrey.slider@gmail.com>
2015-01-19 21:01:40 +0500

    Changes

--- HTML/public_html/local/templates/.default/components/bitrix/news.list/about/template.php
+++ HTML/public_html/local/templates/.default/components/bitrix/news.list/about/template.php
@@ -1,9 +1,9 @@
 <? $this->setFrameMode(true);?>
-<div data-width="100%" data-height="100%" data-nav="false" data-arrows="false" data-transition="crossfade" data-click="false" class="about__slider">
+<div data-width="100%" data-height="100%" data-keyboard="true" data-nav="false" data-arrows="false" data-transition="crossfade" data-click="false" class="about__slider">
   <?foreach ($arResult['ITEMS'] as $key=>$item):?>
   <div style="background-image: url(<?=$item['PREVIEW_PICTURE']['SRC']?>)" class="about__slider-item" data-dark="<?=($item['PROPERTIES']['DARK']['VALUE_XML_ID']=='Y'?"Y":"N")?>">
     <div class="about__slider-item-content">
-      <h2 class="about__slider-title"> <span><?=$item['NAME']?></span>
+      <h2 class="about__slider-title"> <span><?=$item['PROPERTIES']['DARK']['VALUE_XML_ID']?><?=$item['NAME']?></span>
         <div class="about__slider-title-after"></div>
         <div class="about__slider-title-before"></div>
       </h2>
