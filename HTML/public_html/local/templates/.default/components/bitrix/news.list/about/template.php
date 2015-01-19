dd1309fe4fac66ab2c9b5a9b53732752f5cb6437
dayAlone <andrey.slider@gmail.com>
2015-01-19 21:15:57 +0500

    CSS

--- HTML/public_html/local/templates/.default/components/bitrix/news.list/about/template.php
+++ HTML/public_html/local/templates/.default/components/bitrix/news.list/about/template.php
@@ -3,7 +3,6 @@
   <?foreach ($arResult['ITEMS'] as $key=>$item):?>
   <div style="background-image: url(<?=$item['PREVIEW_PICTURE']['SRC']?>)" class="about__slider-item" data-dark="<?=($item['PROPERTIES']['DARK']['VALUE_XML_ID']=='Y'?"Y":"N")?>">
     <div class="about__slider-item-content">
-    <?var_dump($item['PROPERTIES']['DARK'])?>
       <h2 class="about__slider-title"> <span><?=$item['NAME']?></span>
         <div class="about__slider-title-after"></div>
         <div class="about__slider-title-before"></div>
