05f686b25de87eaa64672de8c4af6b94284950cb
dayAlone <andrey.slider@gmail.com>
2015-01-20 13:30:25 +0500

    Changes

--- HTML/public_html/include/product_modifier.php
+++ HTML/public_html/include/product_modifier.php
@@ -9,6 +9,11 @@ $arResult['SECTIONS'] = array();
 foreach ($arResult['ITEMS'] as &$item):
 	$brand = $arResult['BRANDS'][$item['PROPERTIES']['BRAND']['VALUE']];
 	
+	$raw = CIBlockElement::GetElementGroups($arResult['ID']);
+	while($data = $raw->GetNext())
+		if(!in_array($data['CODE'], array('all', 'sale')))
+			$arResult['IBLOCK_SECTION_ID'] = $data['ID'];
+
 	$rsPath = GetIBlockSectionPath($arResult['ID'], $item['IBLOCK_SECTION_ID']);
 	$arPath = $rsPath->GetNext();
 	
@@ -31,6 +36,7 @@ while($img = $raw->Fetch()):
 	$small = CFile::ResizeImageGet(CFile::GetFileArray($img['ID']), Array("width" => 400, "height" => 400), BX_RESIZE_IMAGE_PROPORTIONAL, false, Array("name" => "sharpen", "precision" => 15), false, 75);
 	$arResult['IMAGES'][$img['ID']] = array('small'=> $small['src'], 'src'=>"/upload/".$img['SUBDIR']."/".$img['FILE_NAME'], 'h'=>$img['HEIGHT'], 'w'=>$img['WIDTH']);
 endwhile;
+
 foreach ($arResult['ITEMS'] as &$item)
 	foreach ($item['PROPERTIES']['PICTURES']['VALUE'] as &$img)
 		$img = array_merge($arResult['IMAGES'][$img], array('title'=>$item['OLD_NAME']));
