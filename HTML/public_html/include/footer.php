65d3d3631701136d41975e683f72de5d0e190b44
dayAlone <andrey.slider@gmail.com>
2015-01-23 17:42:00 +0500

    Metrika

--- HTML/public_html/include/footer.php
+++ HTML/public_html/include/footer.php
@@ -175,6 +175,37 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/include/form.php');
     </div>
   </div>
 <?endif;?>
+<!-- Yandex.Metrika counter --> 
+<script type="text/javascript"> 
+var yaParams = {/*Здесь параметры визита*/}; 
+</script> 
+
+<script type="text/javascript"> 
+(function (d, w, c) { 
+(w[c] = w[c] || []).push(function() { 
+try { 
+w.yaCounter28060548 = new Ya.Metrika({id:28060548, 
+webvisor:true, 
+clickmap:true, 
+trackLinks:true, 
+accurateTrackBounce:true,params:window.yaParams||{ }}); 
+} catch(e) { } 
+}); 
+
+var n = d.getElementsByTagName("script")[0], 
+s = d.createElement("script"), 
+f = function () { n.parentNode.insertBefore(s, n); }; 
+s.type = "text/javascript"; 
+s.async = true; 
+s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; 
+
+if (w.opera == "[object Opera]") { 
+d.addEventListener("DOMContentLoaded", f, false); 
+} else { f(); } 
+})(document, window, "yandex_metrika_callbacks"); 
+</script> 
+<noscript><div><img src="//mc.yandex.ru/watch/28060548" style="position:absolute; left:-9999px;" alt="" /></div></noscript> 
+<!-- /Yandex.Metrika counter --> 
 <?$APPLICATION->ShowViewContent('footer');?>
 </body>
 </html>
