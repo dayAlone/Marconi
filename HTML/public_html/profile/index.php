bbae4bac31abf73df1179139aea7449762419a33
dayAlone <andrey.slider@gmail.com>
2015-01-24 20:13:08 +0500

    Profile

--- HTML/public_html/profile/index.php
+++ HTML/public_html/profile/index.php
@@ -5,26 +5,6 @@ $APPLICATION->SetTitle('Личный кабинет');
 ?>
 <div class="page">
     <div class="container">
-        <?if(isset($_REQUEST['ID'])):?>
-        <?$APPLICATION->IncludeComponent("bitrix:sale.personal.order.detail","",Array(
-                "PATH_TO_LIST" => "order_list.php",
-                "PATH_TO_CANCEL" => "order_cancel.php",
-                "PATH_TO_PAYMENT" => "payment.php",
-                "ID" => $_REQUEST['ID'],
-                "CACHE_TYPE" => "A",
-                "CACHE_TIME" => "3600",
-                "CACHE_GROUPS" => "Y",
-                "SET_TITLE" => "Y",
-                "ACTIVE_DATE_FORMAT" => "d.m.Y",
-                "PREVIEW_PICTURE_WIDTH" => "110",
-                "PREVIEW_PICTURE_HEIGHT" => "110",
-                "RESAMPLE_TYPE" => "1",
-                "CUSTOM_SELECT_PROPS" => array(),
-                "PROP_1" => Array(),
-                "PROP_2" => Array()
-            )
-        );?>
-        <?else:?>
         <div class="row">
             <div class="col-xs-5">
                 <?$APPLICATION->IncludeComponent(
@@ -73,7 +53,6 @@ $APPLICATION->SetTitle('Личный кабинет');
             );?>
             </div>
         </div>
-        <?endif?>
     </div>
 </div>
 
