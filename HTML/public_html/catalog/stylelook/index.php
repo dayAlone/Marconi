5bdbc0fe26b3731007a20f29d4e2c692bd650f46
dayAlone <andrey.slider@gmail.com>
2015-01-27 00:09:13 +0500

    Changes

--- HTML/public_html/catalog/stylelook/index.php
+++ HTML/public_html/catalog/stylelook/index.php
@@ -2,27 +2,18 @@
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 $APPLICATION->SetPageProperty('body_class', "lookbook");
 $APPLICATION->SetTitle('Stylelook');
-?>
-<div class="page">
-    <div class="container">
-        <?
-        	
-			$APPLICATION->IncludeComponent("bitrix:news.list", "lookbook", 
-				array(
-					"IBLOCK_ID"      => 7,
-					"NEWS_COUNT"     => "99999909",
-					"SORT_BY1"       => "SORT",
-					"SORT_ORDER1"    => "ASC",
-					"DETAIL_URL"     => "/lookbook/",
-					"CACHE_TYPE"     => "A",
-					'PROPERTY_CODE'  => array('PRODUCTS','VIDEO'),
-					"SET_TITLE"      => "N"
-				),
-				false
-			);
-		?>
-	</div>
-</div>
-<?
+$APPLICATION->IncludeComponent("bitrix:news.list", "lookbook", 
+	array(
+		"IBLOCK_ID"      => 7,
+		"NEWS_COUNT"     => "99999909",
+		"SORT_BY1"       => "SORT",
+		"SORT_ORDER1"    => "ASC",
+		"DETAIL_URL"     => "/lookbook/",
+		"CACHE_TYPE"     => "A",
+		'PROPERTY_CODE'  => array('PRODUCTS','VIDEO'),
+		"SET_TITLE"      => "N"
+	),
+	false
+);
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
 ?>
--- HTML/public_html/local/templates/.default/components/bitrix/news.list/lookbook/template.php
+++ HTML/public_html/local/templates/.default/components/bitrix/news.list/lookbook/template.php
@@ -1,5 +1,7 @@
 <? $this->setFrameMode(true);?>
+<?if(isset($arResult['SECTION']['PATH'][0]['NAME'])):?>
 <h1 class="lookbook__title"><span><?=$arResult['SECTION']['PATH'][0]['NAME']?></span></h1>
+<?endif;?>
 <div style="background-image: url(<?=$arResult['ITEMS'][count($arResult['ITEMS'])-1]['PREVIEW_PICTURE']['SRC']?>)" data-direction="&lt;" class="lookbook__slider-preview lookbook__slider-preview--prev"><?=svg('slider-arrow-left')?></div>
 <div style="background-image: url(<?=$arResult['ITEMS'][1]['PREVIEW_PICTURE']['SRC']?>)" data-direction="&gt;" class="lookbook__slider-preview lookbook__slider-preview--next"><?=svg('slider-arrow-right')?></div>
 <div data-loop="true" data-width="120%" data-keyboard="true" data-nav="false" data-arrows="false" data-click="false" data-transition="crossfade" class="lookbook__slider">
