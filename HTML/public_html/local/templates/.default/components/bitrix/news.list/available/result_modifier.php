d450993237fce97d1d67b48962f6a30edcb846e0
dayAlone <andrey.slider@gmail.com>
2015-01-27 22:52:50 +0500

    Changes

--- HTML/public_html/local/templates/.default/components/bitrix/news.list/available/result_modifier.php
+++ HTML/public_html/local/templates/.default/components/bitrix/news.list/available/result_modifier.php
@@ -3,10 +3,11 @@
 		$arResult['COUNTS'] = array();
 		$raw = CCatalogStoreProduct::GetList(array(), array('PRODUCT_ID' => ($arParams['OFFERS']?array_keys($arParams['OFFERS']):$arParams['CACHE_NOTES'])), false, false);
 		while ($item = $raw->Fetch()):
-			if(!isset($arResult['COUNTS'][$item['STORE_ID']]))
-				$arResult['COUNTS'][$item['STORE_ID']] = array();
-			if($item['AMOUNT']>0)
+			if(intval($item['AMOUNT'])>0):
+				if(!isset($arResult['COUNTS'][$item['STORE_ID']]))
+					$arResult['COUNTS'][$item['STORE_ID']] = array();
 				$arResult['COUNTS'][$item['STORE_ID']][$item['PRODUCT_ID']] = $item['AMOUNT'];
+			endif;
 		endwhile;
 	endif;
 ?>
