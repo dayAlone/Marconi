042e20a1c54ecbd8c214b37fdfbf6fee6c3d79e4
dayAlone <andrey.slider@gmail.com>
2015-01-27 13:11:28 +0500

    Cart

--- HTML/public_html/local/templates/.default/components/bitrix/sale.basket.basket.line/.default/ajax_template.php
+++ HTML/public_html/local/templates/.default/components/bitrix/sale.basket.basket.line/.default/ajax_template.php
@@ -1,18 +1,19 @@
 <?
 $frame = $this->createFrame()->begin();
 $frame->setBrowserStorage(true);
-$_SESSION['ELEMENS'] = array();
-foreach($arResult['CATEGORIES']['READY'] as $item)
-	$_SESSION['ELEMENS'][] = $item['PRODUCT_ID'];
-if($arResult['NUM_PRODUCTS']>0):
-	$count = 0;
-	foreach ($arResult['CATEGORIES']['READY'] as $item)
-		$count += $item['QUANTITY'];
-	?>
-	<a href="/basket/" class="cart"><?=svg('cart')?>В корзине: <span class="cart__num"><?=$count?></span> товар<?=BasketNumberWordEndings($count)?></a>
-<? else: ?>
-	<span class="cart"><?=svg('cart')?> Корзина пуста</span>
-<? endif;
+	
+	if($arResult['NUM_PRODUCTS']>0):
+		$count = 0;
+		$_SESSION['ELEMENS'] = array();
+		foreach ($arResult['CATEGORIES']['READY'] as $item):
+			$count += $item['QUANTITY'];
+			$_SESSION['ELEMENS'][] = $item['PRODUCT_ID'];
+		endforeach;
+		?>
+		<a href="/basket/" class="cart"><?=svg('cart')?>В корзине: <span class="cart__num"><?=$count?></span> товар<?=BasketNumberWordEndings($count)?></a>
+	<? else: ?>
+		<span class="cart"><?=svg('cart')?> Корзина пуста</span>
+	<? endif;
 $frame->beginStub();
 ?>
 	<span class="cart"><?=svg('cart')?> Корзина пуста</span>
