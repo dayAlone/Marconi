a6e0f138213f46d88035155871c4c17a54d5275c
dayAlone <andrey.slider@gmail.com>
2015-03-31 19:17:14 +0500

    About

--- HTML/public_html/eng/about/index.php
+++ HTML/public_html/eng/about/index.php
@@ -4,7 +4,7 @@ $APPLICATION->SetPageProperty('body_class', "about");
 $APPLICATION->SetTitle('О бренде');
     $APPLICATION->IncludeComponent("bitrix:news.list", "about", 
     array(
-	    "IBLOCK_ID"           => 3,
+	    "IBLOCK_ID"           => 8,
 	    "NEWS_COUNT"          => "99999",
 	    "SORT_BY1"            => "SORT",
 	    "SORT_ORDER1"         => "ASC",
