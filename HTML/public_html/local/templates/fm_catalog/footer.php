a65cbcb595d937abfa1c8d4b74e15afcd35f141a
dayAlone <andrey.slider@gmail.com>
2015-05-16 03:42:00 +0500

    Footer

--- HTML/public_html/local/templates/fm_catalog/footer.php
+++ HTML/public_html/local/templates/fm_catalog/footer.php
@@ -1,13 +1,14 @@
 <?if(!isset($_REQUEST['short'])):?>
 	</div>
 </div>
-<div class="catalog__card hidden">
-	<img src="/layout/images/card.png" alt=""><br>
-	<span class="catalog__card-text">Только в нашем интернет-магазине скидка на все товары 10% по вашей новой дисконтной карте, которую мы вам дарим.<br></span>
-	<a href="#" class="catalog__card-button">Отлично! Спасибо!</a>
-</div>
-<div class="catalog__card-frame hidden"></div>
-<?
+<?if(SITE_ID == 's1'):?>
+	<div class="catalog__card hidden">
+		<img src="/layout/images/card.png" alt=""><br>
+		<span class="catalog__card-text">Только в нашем интернет-магазине скидка на все товары 10% по вашей новой дисконтной карте, которую мы вам дарим.<br></span>
+		<a href="#" class="catalog__card-button">Отлично! Спасибо!</a>
+	</div>
+	<div class="catalog__card-frame hidden"></div>
+<?endif;
 require($_SERVER['DOCUMENT_ROOT'].'/include/footer.php');
+endif;
 ?>
-<?endif;?>
