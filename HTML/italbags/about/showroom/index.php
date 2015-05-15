4ef06848017a5bd8f5ba04ab3f133e2dc0d6b9e9
dayAlone <andrey.slider@gmail.com>
2015-05-16 02:51:44 +0500

    Changes

--- HTML/italbags/about/showroom/index.php
+++ HTML/italbags/about/showroom/index.php
@@ -1,7 +1,7 @@
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 $APPLICATION->SetPageProperty('body_class', "textpage");
-$APPLICATION->SetTitle('Реквизиты');
+$APPLICATION->SetTitle('Выставочный зал');
 ?>
 <div class="showroom" data-draggable="true"></div>
 <?
---
+++ HTML/italbags/important/information/index.php
@@ -0,0 +1,22 @@
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+$APPLICATION->SetPageProperty('body_class', "about about--fmarconi about--menu");
+$APPLICATION->SetTitle('Полезная информация');
+    $APPLICATION->IncludeComponent("bitrix:news.list", "about", 
+    array(
+	    "IBLOCK_ID"           => 3,
+	    "NEWS_COUNT"          => "99999",
+	    "SORT_BY1"            => "SORT",
+	    "SORT_ORDER1"         => "ASC",
+	    "DETAIL_URL"          => "/about/",
+	    "CACHE_TYPE"          => "A",
+	    "DISPLAY_PANEL"       => "N",
+	    "SET_TITLE"           => "N",
+	    "PROPERTY_CODE"       => array('DARK')
+       ),
+       false
+    );
+?>
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
+?>
