f9a70ef443a86607b050c9141112d3dbc3a6c45f
dayAlone <andrey.slider@gmail.com>
2015-06-07 21:50:50 +0500

    Changes

--- HTML/italbags/catalog/stylelook/index.php
+++ HTML/italbags/catalog/stylelook/index.php
@@ -8,7 +8,7 @@ $APPLICATION->IncludeComponent("bitrix:news.list", "lookbook",
 		"NEWS_COUNT"     => "99999909",
 		"SORT_BY1"       => "SORT",
 		"SORT_ORDER1"    => "ASC",
-		"DETAIL_URL"     => "/lookbook/",
+		"DETAIL_URL"     => "/catalog/stylelook/",
 		"CACHE_TYPE"     => "A",
 		'PROPERTY_CODE'  => array('PRODUCTS','VIDEO'),
 		"SET_TITLE"      => "N"
