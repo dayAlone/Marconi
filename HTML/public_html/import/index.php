1a6f4c468fe06c0f5d9e9d32681dc483caac47c6
dayAlone <andrey.slider@gmail.com>
2015-06-08 15:10:53 +0500

    Changes

--- HTML/public_html/import/index.php
+++ HTML/public_html/import/index.php
@@ -628,7 +628,7 @@
 				    	endif;
 				    	
 				    	// Товары на основном складе
-				    	if($count->getAttribute('store') == 0):
+				    	if(in_array($count->getAttribute('store'), attay(0,1)):
 					    	$updateID = (isset($product['CML2_LINK'])?$product['CML2_LINK']:$id);
 					    	if(intval($amount) > 0 && $product["GENERAL"] != "Y") 
 					    		$updateValue = "Y";
