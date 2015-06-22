e6928dea5ffe0abd5b303f01447537a7ce2d7cae
dayAlone <andrey.slider@gmail.com>
2015-06-22 20:31:42 +0500

    Changes

--- HTML/public_html/local/templates/.default/components/bitrix/news.list/banners/template.php
+++ HTML/public_html/local/templates/.default/components/bitrix/news.list/banners/template.php
@@ -1,11 +1,11 @@
 <? $this->setFrameMode(true);?>
-<? if(count($arResult['ITEMS'])>1): ?>
+<? if(count($arResult['ITEMS']) > 0): ?>
 <div class="catalog__banner">
 <?foreach ($arResult['ITEMS'] as $key=>$item):
 	if(isset($item['PREVIEW_PICTURE']['SRC'])):?>
 	<a href="<?=$item['PROPERTIES']['LINK']['VALUE']?>">
-		<img src="<?=$item['PREVIEW_PICTURE']['SRC']?>" alt="" class="visible-lg">
-		<img src="<?=$item['DETAIL_PICTURE']['SRC']?>" alt="" class="hidden-lg">
+		<?if(isset($item['PREVIEW_PICTURE']['SRC'])):?><img src="<?=$item['PREVIEW_PICTURE']['SRC']?>" alt="" class="visible-lg"><?endif;?>
+		<?if(isset($item['DETAIL_PICTURE']['SRC'])):?><img src="<?=$item['DETAIL_PICTURE']['SRC']?>" alt="" class="hidden-lg"><?endif;?>
 	</a>
 	<?
 	endif;
