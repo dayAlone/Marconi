c723afff519c2e477f6eacf6341b2b3430310cbf
dayAlone <andrey.slider@gmail.com>
2015-06-22 20:30:18 +0500

    Changes

--- HTML/public_html/local/templates/.default/components/bitrix/news.list/banners/template.php
+++ HTML/public_html/local/templates/.default/components/bitrix/news.list/banners/template.php
@@ -2,7 +2,7 @@
 <? if(count($arResult['ITEMS'])>1): ?>
 <div class="catalog__banner">
 <?foreach ($arResult['ITEMS'] as $key=>$item):
-	if(isset($item['PREVIEW_PICTURE']['SRC']):?>
+	if(isset($item['PREVIEW_PICTURE']['SRC'])):?>
 	<a href="<?=$item['PROPERTIES']['LINK']['VALUE']?>">
 		<img src="<?=$item['PREVIEW_PICTURE']['SRC']?>" alt="" class="visible-lg">
 		<img src="<?=$item['DETAIL_PICTURE']['SRC']?>" alt="" class="hidden-lg">
