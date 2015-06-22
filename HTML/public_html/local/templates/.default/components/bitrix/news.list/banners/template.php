0604418f4f86d392b4f836717a36d6d701d3e4cc
dayAlone <andrey.slider@gmail.com>
2015-06-22 20:37:39 +0500

    changes

--- HTML/public_html/local/templates/.default/components/bitrix/news.list/banners/template.php
+++ HTML/public_html/local/templates/.default/components/bitrix/news.list/banners/template.php
@@ -4,7 +4,7 @@
 <?foreach ($arResult['ITEMS'] as $key=>$item):
 	if(isset($item['PREVIEW_PICTURE']['SRC'])):?>
 	<a href="<?=$item['PROPERTIES']['LINK']['VALUE']?>">
-		<?if(isset($item['PREVIEW_PICTURE']['SRC'])):?><img src="<?=$item['PREVIEW_PICTURE']['SRC']?>" alt="" class="visible-lg"><?endif;?>
+		<?if(isset($item['PREVIEW_PICTURE']['SRC'])):?><img src="<?=$item['PREVIEW_PICTURE']['SRC']?>" alt="" <?if(isset($item['PREVIEW_PICTURE']['SRC'])):?>class="visible-lg"<?endif;?>><?endif;?>
 		<?if(isset($item['DETAIL_PICTURE']['SRC'])):?><img src="<?=$item['DETAIL_PICTURE']['SRC']?>" alt="" class="hidden-lg"><?endif;?>
 	</a>
 	<?
