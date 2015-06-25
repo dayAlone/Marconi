e03925f80cbd61c693bd589397acb3a9bc568277
dayAlone <andrey.slider@gmail.com>
2015-06-25 13:06:41 +0500

    Changes

--- HTML/public_html/local/templates/.default/components/bitrix/news.list/team/result_modifier.php
+++ HTML/public_html/local/templates/.default/components/bitrix/news.list/team/result_modifier.php
@@ -1,5 +1,7 @@
 <?
 foreach ($arResult['ITEMS'] as $key=>&$item):
 	$small = CFile::ResizeImageGet(CFile::GetFileArray($item['PREVIEW_PICTURE']['ID']), Array("width" => 400, "height" => 400), BX_RESIZE_IMAGE_PROPORTIONAL, false, Array("name" => "sharpen", "precision" => 15), false, 75);
+	$big = CFile::ResizeImageGet(CFile::GetFileArray($item['PREVIEW_PICTURE']['ID']), Array("width" => 1000, "height" => 1000), BX_RESIZE_IMAGE_PROPORTIONAL, false, Array("name" => "sharpen", "precision" => 15), false, 75);
 	$item['PREVIEW_PICTURE']['SMALL'] = $small['src'];
+	$item['PREVIEW_PICTURE']['SRC'] = $big['src'];
 endforeach;
--- HTML/public_html/local/templates/.default/components/bitrix/news.list/team/template.php
+++ HTML/public_html/local/templates/.default/components/bitrix/news.list/team/template.php
@@ -1,5 +1,5 @@
 <? $this->setFrameMode(true);?>
-<div class="team">
+<div class="team hidden-xs">
   <div class="team__arrow team__arrow--prev"><?=svg('slider-arrow-left')?></div>
   <div class="team__arrow team__arrow--next"><?=svg('slider-arrow-right')?></div>
 <?foreach ($arResult['ITEMS'] as $key=>$item):?>
@@ -24,12 +24,38 @@
   <div class="team__slider">
   <?foreach ($arResult['ITEMS'] as $key=>$item):?>
     <div class="team__preview-frame">
-      <a href="#el-<?=$key?>" class="team__preview" style="background-image: url(<?=$item['PREVIEW_PICTURE']['SRC']?>)">
+      <a href="#el-<?=$key?>" class="team__preview" style="background-image: url(<?=$item['PREVIEW_PICTURE']['SMALL']?>)">
         <div class="team__preview-text"><span><?=$item['NAME']?></span></div>
       </a>
     </div>
   <?endforeach;?>
   </div>
+</div>
+<div class="team-list visible-xs">
+  <?foreach ($arResult['ITEMS'] as $key=>$item):?>
+  <div class="team-list__item">
+    <div class="row">
+      <div class="col-xs-4">
+        <div class="team-list__image" style="background-image: url(<?=$item['PREVIEW_PICTURE']['SMALL']?>)"></div>
+      </div>
+      <div class="col-xs-8">
+        <?if(strlen($item['PROPERTIES']['POSITION']['VALUE'])>0):?>
+          <div class="team__position"><?=$item['PROPERTIES']['POSITION']['VALUE']?></div>
+        <?endif;?>
+          <div class="team__name"><?=$item['NAME']?></div>
+          <div class="team__divider"></div>
+          <div class="team__info">
+            <?if(strlen($item['PROPERTIES']['PHONE']['VALUE'])>0):?>
+              Тел./факс: <?=$item['PROPERTIES']['PHONE']['VALUE']?><br>
+            <?endif;?>
+            <?if(strlen($item['PROPERTIES']['EMAIL']['VALUE'])>0):?>
+              Электронная почта: <a href="mailto:<?=$item['PROPERTIES']['EMAIL']['VALUE']?>"><?=$item['PROPERTIES']['EMAIL']['VALUE']?></a><br>
+            <?endif;?>
+          </div>
+      </div>
+    </div>
+  </div>
+  <?endforeach?>
 </div>
 <script>
   $(function () {
