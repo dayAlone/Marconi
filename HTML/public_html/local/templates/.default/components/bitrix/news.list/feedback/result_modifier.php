7360d5d1f0feb3d099eba9aedd6452fca1f17578
dayAlone <andrey.slider@gmail.com>
2015-07-04 14:36:05 +0500

    Changes

--- HTML/public_html/local/templates/.default/components/bitrix/news.list/feedback/result_modifier.php
+++ HTML/public_html/local/templates/.default/components/bitrix/news.list/feedback/result_modifier.php
@@ -1,6 +1,6 @@
 <?
 	foreach ($arResult['ITEMS'] as $key=>&$item):
-		$middle = CFile::ResizeImageGet(CFile::GetFileArray($item['PREVIEW_PICTURE']['ID']), Array("width" => 800, "height" => 800), BX_RESIZE_IMAGE_PROPORTIONAL, false, Array("name" => "sharpen", "precision" => 15), true, 75);
+		$middle = CFile::ResizeImageGet(CFile::GetFileArray($item['PREVIEW_PICTURE']['ID']), Array("width" => 800, "height" => 800), BX_RESIZE_IMAGE_PROPORTIONAL, true, Array("name" => "sharpen", "precision" => 15), false, 75);
 		$item['IMAGES'] = array( 0 => array('src'=>$middle['src'], 'h'=>$middle['width'], 'w'=>$middle['height']));
 	endforeach;
 ?>
