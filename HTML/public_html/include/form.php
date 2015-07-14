198efa0ddc1d5181f9fc7237b2f2e7b29b218f9e
dayAlone <andrey.slider@gmail.com>
2015-07-15 01:40:02 +0500

    Changes

--- HTML/public_html/include/form.php
+++ HTML/public_html/include/form.php
@@ -1,3 +1,24 @@
+<?
+function getFormFields ($x = false) {
+    global $USER;
+    if($USER->IsAuthorized()):
+        $rsUser          = CUser::GetByID($USER->GetID());
+        $arUser          = $rsUser->Fetch();
+        $arUser['NAME']  = $arUser['NAME'] . " " .$arUser['LAST_NAME'];
+        $arUser['PHONE'] = (strlen($arUser['WORK_PHONE'])>0?$arUser['WORK_PHONE']:$arUser['PERSONAL_PHONE']);
+    endif;
+    ?>
+    <label>представьтесь, пожалуйста</label>
+    <input name="name" type="text" required value="<?=$arUser['NAME']?>">
+    <label>Ваш e-mail</label>
+    <input name="email" type="email" required value="<?=$arUser['EMAIL']?>">
+    <?if($x):?>
+    <label>телефон для связи с вами</label>
+    <input name="phone" type="text" data-parsley-pattern="/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}/" data-parsley-trigger="change" value="<?=$arUser['PHONE']?>">
+    <?endif;?>
+    <?
+}
+?>
 <div id="feedback" tabindex="-1" role="dialog" aria-hidden="true" class="modal fade feedback">
   <div class="modal-dialog feedback__dialog">
     <div class="modal-content"><a data-dismiss="modal" href="#" class="close"><?=svg('close')?></a>
@@ -7,22 +28,17 @@
     </div>
     <form class="feedback__form" data-parsley-validate>
       <input type="hidden" name="group_id" value="5">
-      <label>представьтесь, пожалуйста</label>
-      <input name="name" type="text" required>
-      <label>Ваш e-mail</label>
-      <input name="email" type="email" required>
-      <label>телефон для связи с вами</label>
-      <input name="phone" type="text" data-parsley-pattern="/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}/" data-parsley-trigger="change">
+      <? getFormFields (true) ?>
       <label>ваше сообщение</label>
       <textarea required name="message"></textarea>
       <div class="row">
         <div class="col-xs-5">
           <label class="left">введите данный код</label>
-          
+
           <div class="captcha" style="background-image:url(/include/captcha.php?captcha_sid=<?=$code?>)"></div>
         </div>
         <div class="col-xs-2 no-padding center">
-          
+
           <input type="hidden" name="captcha_code" value="<?=$code?>">
           <a href="#" class="captcha_refresh">
             <?=svg('refresh')?>
@@ -52,20 +68,17 @@
     </div>
     <form class="feedback__form" data-parsley-validate>
       <input type="hidden" name="group_id" value="17">
-      <label>представьтесь, пожалуйста</label>
-      <input name="name" type="text" required>
-      <label>Ваш e-mail</label>
-      <input name="email" type="email" required>
+      <? getFormFields () ?>
       <label>Ваш вопрос</label>
       <textarea required name="message"></textarea>
       <div class="row">
         <div class="col-xs-5">
           <label class="left">введите данный код</label>
-          
+
           <div class="captcha" style="background-image:url(/include/captcha.php?captcha_sid=<?=$code?>)"></div>
         </div>
         <div class="col-xs-2 no-padding center">
-          
+
           <input type="hidden" name="captcha_code" value="<?=$code?>">
           <a href="#" class="captcha_refresh">
             <?=svg('refresh')?>
@@ -95,20 +108,17 @@
     </div>
     <form class="feedback__form" data-parsley-validate>
       <input type="hidden" name="group_id" value="17">
-      <label>представьтесь, пожалуйста</label>
-      <input name="name" type="text" required>
-      <label>Ваш e-mail</label>
-      <input name="email" type="email" required>
+      <? getFormFields () ?>
       <label>Ваш отзыв</label>
       <textarea required name="message"></textarea>
       <div class="row">
         <div class="col-xs-5">
           <label class="left">введите данный код</label>
-          
+
           <div class="captcha" style="background-image:url(/include/captcha.php?captcha_sid=<?=$code?>)"></div>
         </div>
         <div class="col-xs-2 no-padding center">
-          
+
           <input type="hidden" name="captcha_code" value="<?=$code?>">
           <a href="#" class="captcha_refresh">
             <?=svg('refresh')?>
