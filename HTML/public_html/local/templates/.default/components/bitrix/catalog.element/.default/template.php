15896ab1dbb18dd77fc698faf9592312d2752a9c
dayAlone <andrey.slider@gmail.com>
2015-07-20 16:22:55 +0500

    Changes

--- HTML/public_html/local/templates/.default/components/bitrix/catalog.element/.default/template.php
+++ HTML/public_html/local/templates/.default/components/bitrix/catalog.element/.default/template.php
@@ -327,6 +327,16 @@ endif;
 
 		    	<? endif;?>
 		    <?endif;?>
+			<?if(SITE_ID == 's1'):?>
+				<div class="props__item props__item--medium">
+					<div class="props__name">розничные цены</div>
+					<div class="props__value">
+						<span>
+							<?=number_format($arResult['MIN_PRICE']['VALUE'], 0, '.', ' ')?>
+						</span>
+					</div>
+				</div>
+			<?endif;?>
 	        </div>
 
 
