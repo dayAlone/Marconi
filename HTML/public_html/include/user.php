9f7faa00ccd5d618e2ef85464314018f8da1f237
dayAlone <andrey.slider@gmail.com>
2015-09-26 19:59:29 +0500

    Register

--- HTML/public_html/include/user.php
+++ HTML/public_html/include/user.php
@@ -3,28 +3,28 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.ph
 switch ($_REQUEST['action']):
 	case 'register':
 		$APPLICATION->IncludeComponent("bitrix:main.register","ajax",Array(
-                "USER_PROPERTY_NAME" => "", 
-                "SEF_MODE"           => "Y", 
-                "SHOW_FIELDS"        => Array("NAME", "LAST_NAME", 'PERSONAL_GENDER', 'PERSONAL_BIRTHDAY', 'PERSONAL_PHONE', 
-                								'WORK_PHONE', 'LOGIN', 'WORK_COMPANY', 'WORK_NOTES', 'PERSONAL_NOTES', 'ADMIN_NOTES', 'PERSONAL_MOBILE'),
-                "REQUIRED_FIELDS"    => Array("NAME", "LAST_NAME"), 
-                "AUTH"               => "Y", 
-                "USE_BACKURL"        => "Y", 
-                "USE_CAPTCHA"        => "Y", 
-                "SUCCESS_PAGE"       => "", 
-                "SET_TITLE"          => "Y", 
-                "USER_PROPERTY"      => Array(), 
-                "SEF_FOLDER"         => "/", 
+                "USER_PROPERTY_NAME" => "",
+                "SEF_MODE"           => "Y",
+                "SHOW_FIELDS"        => Array("NAME", "LAST_NAME", 'PERSONAL_GENDER', 'PERSONAL_BIRTHDAY', 'PERSONAL_PHONE',
+                								'WORK_PHONE', 'LOGIN', 'WORK_COMPANY', 'WORK_NOTES', 'PERSONAL_NOTES', 'ADMIN_NOTES', 'PERSONAL_MOBILE', 'WORK_DEPARTMENT', 'WORK_FAX'),
+                "REQUIRED_FIELDS"    => Array("NAME", "LAST_NAME"),
+                "AUTH"               => "Y",
+                "USE_BACKURL"        => "Y",
+                "USE_CAPTCHA"        => "Y",
+                "SUCCESS_PAGE"       => "",
+                "SET_TITLE"          => "Y",
+                "USER_PROPERTY"      => Array(),
+                "SEF_FOLDER"         => "/",
                 "VARIABLE_ALIASES"   => Array()
         ));
 		break;
 	case 'login':
-		$APPLICATION->IncludeComponent("bitrix:system.auth.form", "ajax", 
+		$APPLICATION->IncludeComponent("bitrix:system.auth.form", "ajax",
 			Array(
 				"REGISTER_URL"        => "register.php",
 				"FORGOT_PASSWORD_URL" => "",
 				"PROFILE_URL"         => "/profile/",
-				"SHOW_ERRORS"         => "Y" 
+				"SHOW_ERRORS"         => "Y"
 			)
 		);
 		break;
