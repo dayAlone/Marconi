6e6aaa4b3d268f02a8c9ef1c62c333c5b072e7d3
dayAlone <andrey.slider@gmail.com>
2015-12-20 21:55:44 +0500

    Changes

--- HTML/public_html/local/templates/.default/components/bitrix/news.list/stores/template.php
+++ HTML/public_html/local/templates/.default/components/bitrix/news.list/stores/template.php
@@ -1,5 +1,5 @@
 <? $this->setFrameMode(true);?>
-<? if(count($arResult['ITEMS'])>0): 
+<? if(count($arResult['ITEMS'])>0):
 	if(isset($_REQUEST[$arParams["FIELD_NAME"]])):
 		foreach ($arResult['ITEMS'] as $item):
 			if($item['VALUE']==$_REQUEST[$arParams["FIELD_NAME"]])
@@ -12,7 +12,7 @@
 	<div class="dropdown">
 		<a href="#" class="dropdown__trigger"><span class="dropdown__text"><?=(strlen($name)>0?$name:"Выберите пункт самовывоза")?></span><?=svg('arrow')?></a>
 		<span class="dropdown__frame">
-			<a href="#" data-id="" class="dropdown__item">Выберите пункт самовывоза</a>				
+			<a href="#" data-id="" class="dropdown__item">Выберите пункт самовывоза</a>
 			<?
 			foreach ($arResult['ITEMS'] as $item):
 				?><a href="#" class="dropdown__item" data-id="<?=$item['ID']?>"><?=$item['NAME']?></a><?
@@ -33,11 +33,15 @@
 	<?endif;?>
 	<div class="stores-list__description <?=(count($arResult['ITEMS'])==1?"stores-list__description--active":"")?>">
 		<small><strong>адрес пункта самовывоза</strong></small><br>
-		<?foreach ($arResult['ITEMS'] as $key => $item):?>
-			<div data-id="<?=$item['ID']?>" class="stores-list__item <?=($key==0?"stores-list__item--active":"")?>">
+		<?
+		$i = 0;
+		foreach ($arResult['ITEMS'] as $key => $item):?>
+			<div data-id="<?=$item['ID']?>" class="stores-list__item <?=($i==0?"stores-list__item--active":"")?>">
 				<?=html_entity_decode($item['PROPERTIES']['ADDRESS']['VALUE']['TEXT'])?>
 			</div>
-		<?endforeach;?>
+		<?
+		$i++;
+		endforeach;?>
 	</div>
 </div>
 <?endif;?>
