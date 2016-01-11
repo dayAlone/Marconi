ac9c008279e093ddd9bcd952bf991718a70c1b05
dayAlone <andrey.slider@gmail.com>
2016-01-11 21:57:35 +0500

    Product

--- HTML/public_html/local/templates/.default/components/bitrix/catalog.element/.default/template.php
+++ HTML/public_html/local/templates/.default/components/bitrix/catalog.element/.default/template.php
@@ -184,7 +184,7 @@ endif;
 				        array("=PROPERTY_COMING" => "Y", "=PROPERTY_GENERAL" => "Y")
 				    );
 				else:
-					$arFilter["PROPERTY_GENERAL"] = "Y";
+					if ($props['COMING']['VALUE'] !== 'Y') $arFilter["PROPERTY_GENERAL"] = "Y";
 				endif;
 	        	if(strlen($props['ARTNUMBER']['VALUE'])>0):
 		        	$APPLICATION->IncludeComponent("bitrix:news.list", "colors",
