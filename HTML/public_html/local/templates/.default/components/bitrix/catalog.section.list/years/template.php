be2868afae38baa095282e396482faada632cbf1
dayAlone <andrey.slider@gmail.com>
2016-02-12 14:18:08 +0500

    years

--- HTML/public_html/local/templates/.default/components/bitrix/catalog.section.list/years/template.php
+++ HTML/public_html/local/templates/.default/components/bitrix/catalog.section.list/years/template.php
@@ -3,7 +3,7 @@ $this->setFrameMode(true);
 if(count($arResult['SECTIONS'])>1):?>
 <div class="dropdown">
 	<?foreach ($arResult['SECTIONS'] as $key => &$item):
-		if($arParams["CACHE_NOTES"]==$item['ID'] || (!isset($arParams["CACHE_NOTES"]) && $key == 0)):?>
+		if($arParams["CACHE_NOTES"]==$item['ID'] || (intval($arParams["CACHE_NOTES"])==0 && $key == 0)):?>
 		<a href="#" class="dropdown__trigger"><span class="dropdown__text dropdown__text--white"><?=$item['NAME']?></span><?=svg('arrow')?></a>
 		<?
 		endif;
