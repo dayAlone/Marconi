732d6d9d94021e9bb681dac705a840d1e129fbab
dayAlone <andrey.slider@gmail.com>
2016-02-12 14:12:52 +0500

    Changes

--- HTML/public_html/news/index.php
+++ HTML/public_html/news/index.php
@@ -48,7 +48,7 @@ $APPLICATION->SetTitle('Новости');
     		      "IBLOCK_ID"     => $IBLOCK,
     		      "ELEMENT_CODE"  => $_REQUEST['ELEMENT_CODE'],
     		      "CHECK_DATES"   => "N",
-    		      "IBLOCK_TYPE"   => "content_italbags",
+    		      "IBLOCK_TYPE"   => "content_fmarconi",
     		      "SET_TITLE"     => "Y",
     		      "CACHE_TYPE"    => "A",
     		      "PROPERTY_CODE" => array("GALLERY"),
