19c244ba37d2518eb98e55755814dc339a711e6d
dayAlone <andrey.slider@gmail.com>
2016-03-15 16:15:32 +0500

    Changes

--- HTML/public_html/local/templates/.default/components/bitrix/catalog.element/.default/template.php
+++ HTML/public_html/local/templates/.default/components/bitrix/catalog.element/.default/template.php
@@ -605,7 +605,7 @@ $this->SetViewTarget('footer');
 					Помогите своему близкому человеку выбрать для Вас подарок!<br/>
 					Поделитесь в любой социальной сети личным сообщением с тем, от кого хотите получить выбранный Вами подарок.
 				</p>
-				<div class="social-likes social-likes_notext center" data-title="Это будет лучшим подарком для меня!" data-url="http://<?=$_SERVER['SERVER_NAME']?><?=strtok($APPLICATION->GetCurUri(), '?')?>?buyme=Y&ncc=1"><div class="facebook"></div><div class="twitter"></div><div class="vkontakte"></div><div class="odnoklassniki"></div></div>
+				<div class="social-likes social-likes_notext center" data-title="<?=$arResult['NAME']?>" data-url="http://<?=$_SERVER['SERVER_NAME']?><?=strtok($APPLICATION->GetCurUri(), '?')?>?buyme=Y&ncc=1"><div class="facebook"></div><div class="twitter"></div><div class="vkontakte"></div><div class="odnoklassniki"></div></div>
 			</div>
 		</div>
 	</div>
