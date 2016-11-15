96094fe938662fab4f52180e8b4c0206240ee057
dayAlone <andrey.slider@gmail.com>
2016-11-15 12:56:04 +0700

    fix

--- HTML/public_html/local/templates/.default/components/bitrix/news.list/map/template.php
+++ HTML/public_html/local/templates/.default/components/bitrix/news.list/map/template.php
@@ -9,7 +9,7 @@
       foreach ($arResult['ITEMS'] as $key=>$item):
           if ($_REQUEST['ELEMENT_CODE'] === $item['CODE']) $current = $item;
       endforeach;
-      ?><div style="<?=($current ? 'display: none' : '')"><?
+      ?><div style="<?=($current ? 'display: none' : '')?>"><?
           foreach ($arResult['ITEMS'] as $key=>$item):
             if(count(preg_split("/,/", $item['PROPERTIES']['COORDS']['VALUE']))>0 && isset($item['PROPERTIES']['TYPE']['VALUE_XML_ID']))
               $items[$item['ID']] = array('code'=>$item['CODE'], 'coords' => preg_split("/,/", $item['PROPERTIES']['COORDS']['VALUE']), 'type'=>$item['PROPERTIES']['TYPE']['VALUE_XML_ID']);
